b0333678ed2b66a1aa7e26228f2042be4fac4242
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-23 18:05:56 -0500

    Add example interface. But this implementation is tightly coupled to LogToFile.

---
+++ examples/strategy_design_pattern.php
@@ -0,0 +1,39 @@
+<?php
+
+interface Logger {
+    // All implementations of this interface must have a log() as part of the "contract"
+    public function log($data);
+}
+
+class LogToFile implements Logger {
+    public function log($data)
+    {
+        var_dump('Log the data to a file');
+    }
+}
+
+class LogToDatabase implements Logger {
+    public function log($data)
+    {
+        var_dump('Log the data to a database');
+    }
+}
+
+class LogToWebService implements Logger {
+    public function log($data)
+    {
+        var_dump('Log the data to a web service');
+    }
+}
+
+class App {
+
+    public function log($data)
+    {
+        $logger = new LogToFile;
+        $logger->log($data);
+    }
+}
+
+$app = new App;
+$app->log('We want to depend on abstractions rather than concretions');
