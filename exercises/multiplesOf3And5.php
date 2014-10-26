4701b27afb534d10fe9b5ddd5a3e9cb55be02156
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-26 05:19:47 -0500

    Add first draft of Euler Problem 1.

---
+++ exercises/multiplesOf3And5.php
@@ -0,0 +1,38 @@
+
+<?php
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+echo PHP_EOL . "Project Euler, problem 1" . PHP_EOL;
+echo PHP_EOL . "Find the sum of all the multiples of 3 or 5 below 1000" . PHP_EOL;
+
+$total = 0;
+
+function isDuplicateMultiple($number) {
+    if ($number % 15 == 0)
+        return true;
+}
+
+function isMultipleOfThree($number) {
+    if ($number % 3 == 0)
+        return true;
+}
+
+function isMultipleOfFive($number) {
+    if ($number % 5 == 0)
+        return true;
+}
+
+
+for($i=1; $i<1000; $i++) {
+
+    if (isDuplicateMultiple($i)) {
+        $total += $i;
+    } else if (isMultipleOfThree($i)) {
+        $total += $i;
+    } else if (isMultipleOfFive($i)) {
+        $total += $i;
+    }
+}
+
+echo $total . PHP_EOL;
