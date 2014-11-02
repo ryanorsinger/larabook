1d72f02ec74e5d898fb3100e0ac707758d3c209c
ryanorsinger <ryan.orsinger@gmail.com>
2014-11-01 20:22:44 -0500

    Update gulpfile and settings

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,14 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function() {
-    gulp.src('assets/sass/main.scss')
+    gulp.src('app/assets/sass/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('./public/css'));
 });
+
+// Sets up a watcher function to watch for changes and run 'gulp css'
+gulp.task('watch', function() {
+    gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
